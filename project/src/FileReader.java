22a25bd794b23cf85b764f1876df37e738f16431
sagather <samantha.agather@hotmail.com>
2016-12-07 12:27:02 -0800

    added File Reading utilities, will need to change the file name

---
+++ project/src/FileReader.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * Created by bcxtr on 12/7/2016.
+ */
+public class FileReader {
+
+    public static File openFile(String fileName) throws FileNotFoundException{
+
+        File inf = new File(fileName);
+
+        if(inf == null){
+
+            throw new FileNotFoundException("This file was not created");
+
+        }
+
+        return inf;
+
+    }
+
+    public static String[] readFile(File inFile)throws FileNotFoundException{
+
+        Scanner fileScanner = new Scanner(inFile);
+        int i = 0;
+        String[] constructions = null;
+
+        while(fileScanner.hasNextLine()){
+
+            constructions[i++] = fileScanner.nextLine();
+
+        }
+
+        return constructions;
+
+    }
+
+
+}
--- project/src/SampleClient.java
+++ project/src/SampleClient.java
@@ -1,6 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class SampleClient{
 
     public static void main(String[] args){
+
+        //Open file
+        try{
+
+            File file = FileReader.openFile("file");
+            String[] construction = FileReader.readFile(file);
+
+        }
+        catch (FileNotFoundException e){
+
+            System.out.println(e.getMessage());
+
+        }
+
 
 
     }
