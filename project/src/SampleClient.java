3752e0275c5969bd8a96eb95b06f0e05c23fa29b
sagather <samantha.agather@hotmail.com>
2016-12-07 16:10:34 -0800

    added basic UI

--- project/src/SampleClient.java
+++ project/src/SampleClient.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class SampleClient{
 
@@ -17,6 +18,42 @@ public class SampleClient{
             System.out.println(e.getMessage());
 
         }
+
+        displayUI();
+
+
+
+    }
+
+    public static void displayUI(){
+
+        int choice = 0;
+        Scanner kb = new Scanner(System.in);
+
+        System.out.println("------------Air Travel Options---------------");
+        System.out.println("/n1:  Create a new Airport");
+        System.out.println("2:  Create a new Airline");
+        System.out.println("3:  Create a new Flight/n");
+        System.out.println("4:  Display Current State");
+        System.out.println();
+        System.out.println("------------Sea Travel Options----------------");
+        System.out.println("/n5:  Create a new Cruise");
+        System.out.println("6:  Create a new Port");
+        System.out.println("7:  Create a new Trip");
+        System.out.println("8:  Create a new Ship");
+        System.out.println("9:  Display Current State");
+        System.out.println();
+        System.out.println("------Choose your fate (enter 0 to quit)------");
+
+        choice = kb.nextInt();
+
+        while(choice >  9){
+
+            System.out.println("That is not a valid response.");
+            choice = kb.nextInt();
+
+        }
+
 
 
 
