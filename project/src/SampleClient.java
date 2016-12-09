f7616aa662d1db47cf8906b8bee9dc8c2823f075
cs350ostbym <megan.ostby@eagles.ewu.edu>
2016-12-08 22:43:29 -0800

    misc

--- project/src/SampleClient.java
+++ project/src/SampleClient.java
@@ -48,9 +48,9 @@ public class SampleClient{
 
         System.out.println("---------------General UI--------------------");
         System.out.println("\n1:  Create Airport System with input file");
-        System.out.println("2:  Change the price of seats in a flight section");
-        System.out.println("3:  Search for seats");
-        System.out.println("4:  Change seat class pricing for flight destination [ ] and arrival [ ]");
+        System.out.println("2:  Change the price of seats in a flight section (Not Fully Implemented)");
+        System.out.println("3:  Search for seats (Not Fully Implemented)");
+        System.out.println("4:  Change seat class pricing for flight destination [ ] and arrival [ ](Not Fully Implemented) ");
         System.out.println("5:  Book a seat on a flight");
         System.out.println("6:  Book a seat by seat class and window/aisle preference");
         System.out.println("7:  Display Airport System details");
@@ -73,25 +73,23 @@ public class SampleClient{
             c = kb.nextInt();
 
         }
-
         switch(c){
-
             case 0:  System.exit(0);
 
                     break;
             case 1:  createFromFile();
                     break;
-            case 2:
+            case 2: priceChange();
                     break;
-            case 3:  searchSeats();
+            case 3: searchSeats();
                     break;
-            case 4:
+            case 4: changeSeatClassPrice();
                     break;
-            case 5:
+            case 5: //bookSpecific();
                     break;
-            case 6:
+            case 6: //BookPreference();
                     break;
-            case 7:
+            case 7: //DisplayDetails();
                     break;
             case 8:  writeOutput();
                     break;
@@ -117,15 +115,10 @@ public class SampleClient{
                     break;
             case 19:
                     break;
-
             default:
                 System.exit(0);
-
-
         }
-
         displayUI();
-
     }
 
     public static void createFromFile(){
@@ -142,60 +135,44 @@ public class SampleClient{
             File file = FileReader.openFile(fileName);//C:/Users/Megan Ostby/IdeaProjects/DPP2/project/src/input.txt
             ArrayList<String> construction = FileReader.readFile(file);
             String[]  airports = FileParser.parseAirports(construction);
-
             for(String s: airports){
-
                 travel.createTransport(s);
-
             }
-
             construction.remove(0);
             String[] airlines = FileParser.parseAirlines(construction);
             for(String s : airlines){
 
                 travel.createCompany(s);
-
             }
-
             String[] flights = FileParser.parseFlights();
             String[] dates = FileParser.parseDates();
             String[] departures = FileParser.parseDepartures();
             String[] arrivals = FileParser.parseArrivals();
             String[] seats = FileParser.parseSeats();
-
             for(int i = 0; i < flights.length; i++){
-
                 String airlineName;
 
                 if(i < 2){
 
                     airlineName = airlines[0];
-
                 }
                 else{
 
                     airlineName = airlines[1];
-
                 }
-
                 travel.createTravelMethod(airlineName, departures[i], arrivals[i], Integer.parseInt(dates[i]),
                     Integer.parseInt(dates[(i*5)+1]), Integer.parseInt(dates[(i*5)+2]), Integer.parseInt(dates[(i*5)+3]),
                         Integer.parseInt(dates[(i*5)+4]), flights[i]);
 
             }
-
-
-
-
         }
         catch (FileNotFoundException e){
 
             System.out.println("File was not found" + e.getMessage());
-
         }
     }
 
-    public static void searchSeats()
+    public static void searchSeats() //Not fully implemented, If time allowed I would create methods within travel factory to check each
     {
         Scanner kb = new Scanner(System.in);
         System.out.println("Please enter desired seat class: ");
@@ -210,7 +187,18 @@ public class SampleClient{
         int day = kb.nextInt();
         System.out.println("Please enter desired departure year: ");
         int year = kb.nextInt();
+    }
 
+    public static void priceChange() {//Not fully implemented, if time allowed I would search through the given associated prices, and swap them as needed
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Please enter desired section to change the price in (Must be ECONOMY, BUSINESS, or FIRST): ");
+        String seatClass = kb.nextLine();
+        System.out.println("Please enter desired price: ");
+        int price = kb.nextInt();
+    }
+
+    public static void changeSeatClassPrice()
+    {
 
     }
 
